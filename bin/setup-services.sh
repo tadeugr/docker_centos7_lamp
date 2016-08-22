4dd05790a7cb061fa68c1242cd11c0d17233d276
Tadeu Granemann <tadeugr@users.noreply.github.com>
2016-08-22 08:37:26 -0500

    Update setup-services.sh

--- bin/setup-services.sh
+++ bin/setup-services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mysql_install_db
+mysql_install_db > /dev/null 2>&1
 chown -R mysql:mysql /var/lib/mysql
 mysqld_safe --nowatch
-./bin/mysqladmin -u root password 'pwd123'
+mysqladmin -u root password 'pwd123'
